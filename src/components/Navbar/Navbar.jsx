4a46d9411a98b2a9f6e5e67c014637dcf519f352
Kristof Kruller <kristof.kruller@gmail.com>
2021-02-21 14:34:02 +0100

    Ntypo fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, ToolBar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 
 import elogo from '../../assets/elogo.png';
@@ -9,7 +9,7 @@ const Navbar = () => {
     const classes = useStyles();
     return (
         <div>
-            <Appbar position="fixed" className={classes.appBar} color="inherit">
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
                     <Typography variant="h6" className={classes.appBar} color="inherit">
                         <img src={elogo} alt="Commerce.js" height="25px" className={classes.image}/>
@@ -24,7 +24,7 @@ const Navbar = () => {
                         </IconButton>
                     </div>
                 </Toolbar>
-            </Appbar>
+            </AppBar>
         </div>
     )
 }
