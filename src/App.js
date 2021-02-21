6ed595c8073aab64c5d84c7a01ab97c95cf1d6df
Kristof Kruller <kristof.kruller@gmail.com>
2021-02-21 10:21:48 +0100

    products structure and material-ui and styles import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Products from './components/Products';
 
 const App = () => {
     return (
         <div>
-            
+            <Products />
         </div>
     )
 }
---
+++ src/components/Products.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Grid } from '@material-ui/core/';
+import Product from './Product';
+
+const products = [
+    { id:1, name: 'productOne', description: 'This will be the desc.', price: '$', image: '' },
+    { id:2, name: 'productTwo', description: 'This will be the desc.', price: '$', image: '' },
+];
+
+const Products = () => {
+    return (
+        <main> 
+            <Grid container justify='center' spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product} />
+                    </Grid> 
+                ))}
+            </Grid>
+        </main>
+    );
+}
+
+export default Products;
